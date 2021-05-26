0d8d04bb7a572a46b672fe367f9e664c8b71a437
Usuario <Usuario@DESKTOP-IOL192P>
2021-05-26 15:19:29 -0300

    Aula09 finalizada

---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Produto {
+    private String nomeProduto = "";
+    private String descricao = "";
+    private double valorUnit = 0;
+    private int qtd = 0;
+    private int codProduto = 0;
+    private double valorTotal = (valorUnit*qtd);
+    
+    Fornecedor forn = new Fornecedor();
+    
+    private String cnpjFornecedor = forn.getCnpj();
+
+    /**
+     * @return the nomeProduto
+     */
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    /**
+     * @param nomeProduto the nomeProduto to set
+     */
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the valorUnit
+     */
+    public double getValorUnit() {
+        return valorUnit;
+    }
+
+    /**
+     * @param valorUnit the valorUnit to set
+     */
+    public void setValorUnit(double valorUnit) {
+        this.valorUnit = valorUnit;
+    }
+
+    /**
+     * @return the qtd
+     */
+    public int getQtd() {
+        return qtd;
+    }
+
+    /**
+     * @param qtd the qtd to set
+     */
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    /**
+     * @return the codProduto
+     */
+    public int getCodProduto() {
+        return codProduto;
+    }
+
+    /**
+     * @param codProduto the codProduto to set
+     */
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    /**
+     * @return the cnpjFornecedor
+     */
+    public String getCnpjFornecedor() {
+        return cnpjFornecedor;
+    }
+
+    /**
+     * @param cnpjFornecedor the cnpjFornecedor to set
+     */
+    public void setCnpjFornecedor(String cnpjFornecedor) {
+        this.cnpjFornecedor = cnpjFornecedor;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
